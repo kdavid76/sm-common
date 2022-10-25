f12b805e175156d75bdfdf112285fcde2af6afcc
davidk76 <mikehammer1902@gmail.com>
2022-10-25 23:38:36 +0100

    SM-COMMON: Delete Java Code

--- sm-mongo/src/main/java/com/bkk/sm/mongo/authentication/service/ReactiveUserService.java
+++
@@ -1,6 +0,0 @@
-package com.bkk.sm.mongo.authentication.service;
-
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-
-public interface ReactiveUserService extends ReactiveUserDetailsService {
-}
