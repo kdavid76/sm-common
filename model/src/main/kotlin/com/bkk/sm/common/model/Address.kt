d7623c9670a81cde831cd86ffdbb23548cb867c6
kdavid <mikehammer1902@gmail.com>
2022-12-10 01:00:24 +0000

    SM-COMMON: Moving company and user resources to common model

---
+++ model/src/main/kotlin/com/bkk/sm/common/model/Address.kt
@@ -0,0 +1,12 @@
+package com.bkk.sm.common.model
+
+data class Address(
+        val city: String,
+        val postCode: Int,
+        val streetName: String,
+        val areaType: AreaType,
+        val houseNumber: String,
+        val level: Int?,
+        val door: Int?,
+        val locationCode: String?
+)
